11f3bfbaa85ef15e2f449bf668c07ec1ada16a1f
Niels Smits <49674820+NielsSmits1@users.noreply.github.com>
2021-02-08 10:20:24 +0100

    Week 2 listview

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -5,25 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    static final String EXTRA_MESSAGE = "EXTRA_MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] myStringArray = { "Bulbasaur", "Dragonite", "Pikachu" } ;
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, myStringArray);
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+        AdapterView.OnItemClickListener mMessageClickedHandler = new
+                AdapterView.OnItemClickListener() {
+                    public void onItemClick(AdapterView parent, View v, int position, long
+                            id) {
+                        String message = adapter.getItem(position);
+                        Toast.makeText(MainActivity.this, message,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                };
+        listView.setOnItemClickListener(mMessageClickedHandler);
     }
 
-    public void sendMessage(View view) {
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        Intent sendIntent = new Intent();
-        sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.putExtra(Intent.EXTRA_TEXT, message);
-        sendIntent.setType("text/plain");
-        startActivity(sendIntent);
-    }
 
 }
