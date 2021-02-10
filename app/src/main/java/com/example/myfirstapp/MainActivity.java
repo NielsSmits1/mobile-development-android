ca24898a3ac24076bdc3be658e0c1d970fe44987
Niels Smits <49674820+NielsSmits1@users.noreply.github.com>
2021-02-10 22:23:55 +0100

    Workshop 3 deel 3 | toolbar

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,58 +1,139 @@
 package com.example.myfirstapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements OverviewFragment.OnClickListener{
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentManager manager = getSupportFragmentManager();
+        Toolbar toolbar = findViewById(R.id.toolbar);
 
-        boolean portrait = getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+        setSupportActionBar(toolbar);
 
-        if(portrait){
-            manager.beginTransaction().add(R.id.container, new OverviewFragment(), "overview").commit();
-        }
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        Spinner spinner = findViewById(R.id.spinner);
+
+        String[] pokemon = {"Bulbasaur", "Dragonite", "Pikachu"};
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, pokemon);
+
+        spinner.setAdapter(arrayAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String pokemon = arrayAdapter.getItem(position);
+                setPokemon(pokemon);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.toolbar_favorite){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setIcon(R.mipmap.ic_launcher);
+            builder.setTitle("Title");
+            builder.setMessage("Message");
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(MainActivity.this, "Positive", Toast.LENGTH_LONG).show();
+                }
+            });
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(MainActivity.this, "Negative", Toast.LENGTH_LONG).show();
+                }
+            });
+            builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(MainActivity.this, "Neutral", Toast.LENGTH_LONG).show();
+                }
+            });
+            builder.create().show();
+        }
+        return true;
+    }
 
     @Override
-    public void onItemSelected(String pokemon) {
-        boolean portrait = getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
-        if(portrait){
-            FragmentManager manager = getSupportFragmentManager();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
 
-            DetailFragment fragment = new DetailFragment();
+        SearchView searchView = (SearchView) menu.findItem(R.id.toolbar_search).getActionView();
 
-            fragment.setArguments(new Bundle());
-            fragment.getArguments().putString("pokemon", pokemon);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                setPokemon(query);
+                return true;
+            }
 
-            manager.beginTransaction().replace(R.id.container, fragment, "detail").addToBackStack(null).commit();
-        }else{
-            DetailFragment detailFragment = (DetailFragment) getSupportFragmentManager().findFragmentById(R.id.detailFragment);
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return true;
+    }
 
-            detailFragment.setPokemon(pokemon);
+    void setPokemon(String pokemon){
+        TextView textView = findViewById(R.id.textView);
+        ImageView imageView = findViewById(R.id.imageView);
+
+        switch (pokemon){
+            case "Bulbasaur":
+                textView.setText(pokemon);
+                imageView.setImageResource(R.drawable.bulbasaur);
+                break;
+            case "Dragonite":
+                textView.setText(pokemon);
+                imageView.setImageResource(R.drawable.dragonite);
+                break;
+            case "Pikachu":
+                textView.setText(pokemon);
+                imageView.setImageResource(R.drawable.pikachu);
+                break;
         }
-
     }
 }
